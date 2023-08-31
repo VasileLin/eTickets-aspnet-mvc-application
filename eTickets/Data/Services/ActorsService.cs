5adeff5380dcf064da5e988d6892e1b7d08909c3
VasileLin <vasile.linga1@gmail.com>
2023-08-31 09:51:18 +0300

    Adding ActorService and Create Functionality

---
+++ eTickets/Data/Services/ActorsService.cs
@@ -0,0 +1,42 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Services
+{
+    public class ActorsService : IActorsService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ActorsService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task Add(Actor actor)
+        {
+            _dbContext.Actors.Add(actor);
+            _dbContext.SaveChanges();
+            return Task.CompletedTask;
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Actor>> GetAll()
+        {
+            return await _dbContext.Actors.ToListAsync();
+        }
+
+        public Task<Actor> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Actor> Update(int id, Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
